d2183aec9922ab53dd124d6f07568edddc996c27
Matej Križanec <matej.krizanec97@gmail.com>
2020-05-25 22:33:14 +0200

    database

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -1,33 +1,20 @@
-<?php 
+<?php
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class SearchController extends AbstractController {
-
+class SearchController extends AbstractController
+{
+    /**
+     * @Route("/", name="search")
+     */
     public function index()
     {
-        /**
-         * @Route("/")
-         * @Method({"GET", "POST"})
-         */
-        return $this->render('search/index.html.twig');
-    }
-
-    public function call()
-    {
-
+        return $this->render('search/index.html.twig', [
+            'controller_name' => 'SearchController',
+        ]);
     }
 }
-
-
-
-
-
-
-
-
---
+++ src/Entity/Records.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecordsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RecordsRepository::class)
+ */
+class Records
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $term;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $results;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $datetime_created;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=0, nullable=true)
+     */
+    private $mark;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTerm(): ?string
+    {
+        return $this->term;
+    }
+
+    public function setTerm(?string $term): self
+    {
+        $this->term = $term;
+
+        return $this;
+    }
+
+    public function getResults(): ?int
+    {
+        return $this->results;
+    }
+
+    public function setResults(?int $results): self
+    {
+        $this->results = $results;
+
+        return $this;
+    }
+
+    public function getDatetimeCreated(): ?\DateTimeInterface
+    {
+        return $this->datetime_created;
+    }
+
+    public function setDatetimeCreated(?\DateTimeInterface $datetime_created): self
+    {
+        $this->datetime_created = $datetime_created;
+
+        return $this;
+    }
+
+    public function getMark(): ?string
+    {
+        return $this->mark;
+    }
+
+    public function setMark(?string $mark): self
+    {
+        $this->mark = $mark;
+
+        return $this;
+    }
+}
