1238b01c5091aac036ad64e8c8a6943f0fa77634
Matej Križanec <matej.krizanec97@gmail.com>
2020-05-25 19:35:42 +0200

    controller

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class SearchController {
+
+    public function index()
+    {
+        return new Response('<h1>Hi</h1>');
+    }
+
+}
+
+
+
+
+
+
+
+
