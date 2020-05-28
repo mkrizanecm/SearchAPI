dfa9413376fabb1f16d04bd010e4d834f688bae6
Matej Križanec <matej.krizanec97@gmail.com>
2020-05-28 17:29:16 +0200

    forms

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\{TextType};
+
 
 class SearchController extends AbstractController
 {
@@ -16,7 +18,17 @@ class SearchController extends AbstractController
         return $this->render('search/index.html.twig', [
             'controller_name' => 'SearchController',
         ]);
+    }
 
+    public function searchForm()
+    {
+        $form = $this->createFormBuilder(null)
+            ->add('term', TextType::class)
+            ->getForm();
+
+        return $this->render('search/search.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     public static function call()
