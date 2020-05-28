8bbaa1782ae388169f9b9a28d5d5e5f0bb75d536
Matej Križanec <matej.krizanec97@gmail.com>
2020-05-28 18:09:30 +0200

    search term

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\{TextType};
 
@@ -13,8 +14,15 @@ class SearchController extends AbstractController
     /**
      * @Route("/", name="search")
      */
-    public function index()
+    public function index(Request $response)
     {
+        // Check if there is a POST request
+        $search_term = $response->request->get('form');
+
+        if (!empty($search_term)) {
+            $term = $search_term['term'];
+        }
+
         $form = $this->createFormBuilder(null)
             ->add('term', TextType::class)
             ->getForm();
